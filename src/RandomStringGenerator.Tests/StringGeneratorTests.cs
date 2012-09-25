89537afa0341bf8b4cb4a001eb98f76dc85bfcd2
Petro Sidlovskyy <sidlovskyy@gmail.com>
2012-09-26 02:18:52 +0300

    * String length and length range functionalities added.

---
+++ src/RandomStringGenerator.Tests/StringGeneratorTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace RandomStringGenerator.Tests
+{
+    [TestFixture]
+    class StringGeneratorTests
+    {
+        [Test]
+        public void Generate_WithoutConfiguration_RetursNotEmptyString()
+        {
+            new StringGenerator().Generate()
+                .Should()
+                .NotBeNullOrEmpty();
+        }
+
+        [Test]
+        public void Generate_WithLengthScpecified_RetursStringWithExactLength()
+        {
+            const int length = 5;
+            new StringGenerator().WithLength(length).Generate()
+                .Should()
+                .HaveLength(length);
+        }
+
+        [Test]
+        public void Generate_WithZeroOrNegativeLength_ThrowsArgumetException()
+        {
+            const int length = 0;
+
+            //act
+            Action invalidLengthSpecified = 
+                () => new StringGenerator().WithLength(length).Generate();
+
+            //assert
+            invalidLengthSpecified.ShouldThrow<ArgumentOutOfRangeException>()
+               .And.ActualValue.Should().Be(length);
+        }
+
+        [Test]
+        public void Generate_WithLengthRangeScpecified_RetursStringWithLengthInThatRange()
+        {
+            const int min = 17;
+            const int max = 21;
+            new StringGenerator()
+                .WithLengthRange(min, max).Generate().Length
+                .Should()
+                .BeGreaterOrEqualTo(min).And.BeLessOrEqualTo(max);
+        }
+
+        [Test]
+        public void Generate_WithZeroOrNegativeRangeBorders_ThrowsArgumetException()
+        {
+            const int min = -1;
+            const int max = 21;
+
+            //act
+            Action invalidLengthSpecified = 
+                () => new StringGenerator().WithLengthRange(min, max).Generate();
+
+            //assert
+            invalidLengthSpecified.ShouldThrow<ArgumentException>()
+                .WithMessage("Range elements should be greater then zero");
+        }
+
+        [Test]
+        public void Generate_WithInvalidRange_ThrowsArgumetException()
+        {
+            const int min = 45;
+            const int max = 42;
+
+            //act
+            Action invalidLengthSpecified =
+                () => new StringGenerator().WithLengthRange(min, max).Generate();
+
+            //assert
+            invalidLengthSpecified.ShouldThrow<ArgumentException>()
+                .WithMessage("Lower boundary should be less or equals than higher");
+        }
+    }
+}
---
+++ src/RandomStringGenerator/StringGenerator.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace RandomStringGenerator
+{
+    public class StringGenerator
+    {
+        private const int MAX_DEFAULT_STRING_LENGTH = 1000;
+
+        private static readonly Random _rand = new Random(DateTime.Now.Millisecond);
+
+        private int _length;
+        private int _minLength;
+        private int _maxLength;        
+
+        public string Generate()
+        {
+            if (LengthRangeIsSpecified)
+            {
+                _length = _rand.Next(_minLength, _maxLength);
+            }
+            else
+            if (!LengthIsSpecified)
+            {
+                _length = _rand.Next(1, MAX_DEFAULT_STRING_LENGTH);
+            }
+
+            string result = string.Empty;
+
+            for (int i = 0; i < _length; i++)
+            {
+                result += ' ';
+            }
+
+            return result;
+        }        
+
+        public StringGenerator WithLength(int length)
+        {
+            if(length <= 0)
+            {
+                throw new ArgumentOutOfRangeException("length", length, "Length should be greater then zero");
+            }
+
+            _length = length;
+            ClearLengthRange();
+
+            return this;
+        }
+
+        public StringGenerator WithLengthRange(int min, int max)
+        {
+            if (min <= 0 || max <= 0)
+            {
+                throw new ArgumentException("Range elements should be greater then zero");
+            }
+
+            if(min > max)
+            {
+                throw new ArgumentException("Lower boundary should be less or equals than higher");
+            }
+
+            _minLength = min;
+            _maxLength = max;
+            ClearLength();
+
+            return this;
+        }
+
+        private bool LengthRangeIsSpecified
+        {
+            get
+            {
+                return (_minLength > 0) && (_maxLength > 0);
+            }
+        }
+
+        private bool LengthIsSpecified
+        {
+            get
+            {
+                return _length > 0;
+            }
+        }
+
+        private void ClearLength()
+        {
+            _length = 0;
+        }
+
+        private void ClearLengthRange()
+        {
+            _minLength = 0;
+            _maxLength = 0;
+        }
+
+        private void SetSameMinMaxLength(int lenth)
+        {
+            _maxLength = _minLength = lenth;
+        }
+    }
+}
